08ed05f05a1e9308f6c7f6474dee7ae7474552b8
Mahi Patel <mahipatel72754@gmail.com>
2026-06-10 22:06:11 +0530

    fix admin dashboard api urls

--- Frontend/src/Pages/AdminDashboard.jsx
+++ Frontend/src/Pages/AdminDashboard.jsx
@@ -32,7 +32,7 @@ function AdminDashboard() {
   const fetchContacts = async () => {
     try {
       const res = await axios.get(
-  "https://stepway-backend.vercel.app/api/contact"
+ "https://stepway-backend.vercel.app/api/contact"
       );
 
       setContacts(res.data);
