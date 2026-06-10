c22426f6b1996537d46e22b029d8ad0e074f47ba
Mahi Patel <mahipatel72754@gmail.com>
2026-06-10 08:54:34 +0530

    Add DB port for Railway

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -7,11 +7,11 @@ console.log("DB =", process.env.DB_NAME);
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
-
 db.connect((err) => {
   if (err) {
     console.error("❌ Database connection failed:");
