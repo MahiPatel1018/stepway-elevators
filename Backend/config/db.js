568128f71190722d5c7cee3f39a197f113f802d4
Mahi Patel <mahipatel72754@gmail.com>
2026-06-10 09:10:38 +0530

    Use MySQL pool

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,21 +5,17 @@ console.log("USER =", process.env.DB_USER);
 console.log("HOST =", process.env.DB_HOST);
 console.log("DB =", process.env.DB_NAME);
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
-db.connect((err) => {
-  if (err) {
-    console.error("❌ Database connection failed:");
-    console.error(err.message);
-    return;
-  }
 
-  console.log("✅ MySQL Connected");
-});
+console.log("✅ MySQL Pool Created");
 
 module.exports = db;
